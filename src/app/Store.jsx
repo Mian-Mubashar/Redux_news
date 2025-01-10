8703224f0f11d39d57a06ce020bbb08f59b0d68b
Mian-Mubashar <mubasharhanif24@gmail.com>
2025-01-10 18:31:04 +0500

    2nd

--- src/app/Store.jsx
+++ src/app/Store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import newsReducer from "../features/news/newsSlice";
+import newsReducer from "../features/news/NewsSlice";
 
 export const store = configureStore({
   reducer: {
